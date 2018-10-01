9ddb4803841f7e988e564a5ff4ed739f8981c9b0
zerogpm <zerogpm@gmail.com>
2018-10-01 16:27:09 -0700

    reactive route

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,8 @@
 <template>
     <ul class="nav nav-pills">
         <router-link tag="li" to="/" active-class="active" exact=""><a>Home</a></router-link>
-        <router-link tag="li" to="/user/10" active-class="active"><a>User</a></router-link>
+        <router-link tag="li" to="/user/1" active-class="active"><a>User 1</a></router-link>
+        <router-link tag="li" to="/user/2" active-class="active"><a>User 2</a></router-link>
         <router-link tag="li" to="/about" active-class="active"><a>About Us</a></router-link>
     </ul>
 </template>
