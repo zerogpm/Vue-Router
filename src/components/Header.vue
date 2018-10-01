e077b2987317177bd9e4c8e714be1dcae7307c00
zerogpm <zerogpm@gmail.com>
2018-10-01 15:48:12 -0700

    add active class

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
     <ul class="nav nav-pills">
-        <router-link tag="li" to="/"><a>Home</a></router-link>
-        <router-link tag="li" to="/user"><a>User</a></router-link>
-        <router-link tag="li" to="/about"><a>About Us</a></router-link>
+        <router-link tag="li" to="/" active-class="active" exact=""><a>Home</a></router-link>
+        <router-link tag="li" to="/user" active-class="active"><a>User</a></router-link>
+        <router-link tag="li" to="/about" active-class="active"><a>About Us</a></router-link>
     </ul>
 </template>
 
