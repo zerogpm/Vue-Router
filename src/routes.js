fb017e5fe5da3806a3b67c934ba2a4858733cf0c
zerogpm <zerogpm@gmail.com>
2018-10-01 19:19:42 -0700

    route-view name

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,20 @@
 import User from './components/user/User'
 import Home from './components/Home'
+import Header from './components/Header'
 import About from './components/About'
 import UserDetail from './components/user/UserDetail'
 import UserStart from './components/user/UserStart'
 import UserEdit from './components/user/UserEdit'
 
 export const routes = [
-  { path: '', component: Home, name: "home" },
-  { path: '/user', component: User, children: [
+  { path: '', component: Home, name: "home", components: {
+      default: Home,
+      'header-top' : Header
+    } },
+  { path: '/user', components: {
+      default: User,
+      'header-bottom' : Header
+    }, children: [
       { path: '', component: UserStart },
       { path: ':id', component: UserDetail },
       { path: ':id/edit', component: UserEdit, name: 'userEdit' },
