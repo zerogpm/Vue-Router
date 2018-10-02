569c0892ce62e60e8c3e19053fa25ad2c148a5fc
zerogpm <zerogpm@gmail.com>
2018-10-01 22:15:13 -0700

    scroll behavior with route

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -4,6 +4,8 @@
         <p>Edit User ID: {{ $route.params.id }}</p>
         <p>Locale: {{ $route.query.locale }}</p>
         <p>value: {{ $route.query.q }}</p>
+        <div style="height: 700px"></div>
+        <p id="data">some extra Data</p>
     </div>
 </template>
 
