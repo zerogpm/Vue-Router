fa7f1380d9542a84c9fc170e719f32c35b351ad4
zerogpm <zerogpm@gmail.com>
2018-10-01 22:39:27 -0700

    using beforeleave

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -4,7 +4,7 @@
         <p>Edit User ID: {{ $route.params.id }}</p>
         <p>Locale: {{ $route.query.locale }}</p>
         <p>value: {{ $route.query.q }}</p>
-        <button class="btn btn-primary">Confirm</button>
+        <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
         <div style="height: 700px"></div>
         <p id="data">some extra Data</p>
     </div>
@@ -12,7 +12,22 @@
 
 <script>
   export default {
-    name: "UserEdit"
+    data () {
+      return {
+        confirmed: false
+      }
+    },
+    beforeRouteLeave(to, from, next) {
+      if (this.confirmed) {
+        next();
+      } else {
+        if (confirm('Are you sure?')) {
+          next()
+        } else {
+          next(false);
+        }
+      }
+    }
   }
 </script>
 
